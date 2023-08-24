befbc79cb7a12b5b4f11f8110d091225222deafc
高橋幸樹 <takahashik4649@gmail.com>
2023-08-25 00:56:41 +0900

    add complete screen, register ui and change manifest, next i will scroll, delete and compare enter, databade

--- app/src/main/java/com/example/cost_application/Complete.java
+++ app/src/main/java/com/example/cost_application/Complete.java
@@ -1,38 +1,37 @@
 package com.example.cost_application;
-import android.view.View;
-
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
 
-public class Register extends AppCompatActivity implements View.OnClickListener{
+public class Complete extends AppCompatActivity implements View.OnClickListener{
     TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_complete);
         //add button listener
-        findViewById(R.id.regTomain_button).setOnClickListener(this);
-        findViewById(R.id.done_button).setOnClickListener(this);
-        t= findViewById(R.id.title_register_text);
+        findViewById(R.id.completeTomain_button).setOnClickListener(this);
+        findViewById(R.id.completeTocom_button).setOnClickListener(this);
+        t= findViewById(R.id.title_complete_text);
     }
 
     //    @Override
     //button push process
     public void onClick(View v) {
-        if (v.getId() == R.id.regTomain_button) {
+        if (v.getId() == R.id.completeTomain_button) {
             //register button push process
             startActivity(new Intent(this, MainActivity.class));
-        }else if (v.getId() == R.id.done_button) {
+        } else if (v.getId() == R.id.completeTocom_button) {
             //register button push process
-            startActivity(new Intent(this, Complete.class));
+            startActivity(new Intent(this, Compare.class));
         }
         //名残
         Snackbar.make(v, "ボタンが押されました", Snackbar.LENGTH_SHORT).show();
         //
     }
 }
-
