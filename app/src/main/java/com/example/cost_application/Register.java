1b57adf5deab7d337fc2edfa4fd343f7603d7dba
高橋幸樹 <takahashik4649@gmail.com>
2023-08-26 23:32:31 +0900

    before database add update

--- app/src/main/java/com/example/cost_application/Register.java
+++ app/src/main/java/com/example/cost_application/Register.java
@@ -4,12 +4,14 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class Register extends AppCompatActivity implements View.OnClickListener{
     TextView t;
+    EditText shop_name_data,category_data,name_data,cost_data,date_data,unit_data,remark_data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +20,13 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
         findViewById(R.id.regTomain_button).setOnClickListener(this);
         findViewById(R.id.done_button).setOnClickListener(this);
         t= findViewById(R.id.title_register_text);
+        shop_name_data= findViewById(R.id.edit_shopname);
+        category_data= findViewById(R.id.edit_category);
+        name_data= findViewById(R.id.edit_name);
+        cost_data= findViewById(R.id.edit_cost);
+        date_data= findViewById(R.id.edit_date);
+        unit_data= findViewById(R.id.edit_unit);//unit_data.getText().toString();で取得可能
+        remark_data= findViewById(R.id.edit_remark);
     }
 
     //    @Override
